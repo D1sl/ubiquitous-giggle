7f322617ce4803eea4ce717c8a8ae738f8976d52
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-07 21:59:09 -0600

    added comments to modularised noteRoutes.js

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,10 +3,12 @@ const { notes } = require('../../db/db');
 const { findById, removeNote, createNewNote } = require('../../lib/notes');
 const uuid = require('uuid');
 
+// Main API endpoint
 router.get('/notes', (req, res) => {
     res.json(notes);
 }); 
 
+// Get notes based on their unique ID
 router.get('/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
@@ -16,6 +18,7 @@ router.get('/notes/:id', (req, res) => {
     }
 })
 
+// API endpoint for POST requests
 router.post('/notes', (req, res) => {
     req.body.id = uuid.v4();
     console.log(notes);
@@ -24,6 +27,12 @@ router.post('/notes', (req, res) => {
 
 });
 
+// Wildcard - serves index.html when the user enters an address that doesn't match any of the other HTML routes.
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+})
+
+// API request for deleting entries
 router.delete('/notes/:id', (req, res) => {
     removeNote(req.params.id, notes);
     res.json();
