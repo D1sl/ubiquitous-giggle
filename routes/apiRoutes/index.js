9928668314e9c8c68f877ee86c999549c659f376
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-07 22:09:07 -0600

    added more comments

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const noteRoutes = require('../apiRoutes/noteRoutes')
 
+// Tells router to use noteRoutes instead of index.js
 router.use(noteRoutes);
 
 module.exports = router;
