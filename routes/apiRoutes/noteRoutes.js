a16babf0cfd309398b795ee9e52e1fc5b6560ed0
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-07 21:57:45 -0600

    modularised the api routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,13 +1,32 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db'); 
-const saveNoteData = require('../../lib/notes')
+const { findById, removeNote, createNewNote } = require('../../lib/notes');
+const uuid = require('uuid');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 }); 
 
-router.post('db/db', (req, res) => {
-    console.log(req.body)
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 })
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuid.v4();
+    console.log(notes);
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+    removeNote(req.params.id, notes);
+    res.json();
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const uuid = require('uuid');
 const { findById, removeNote, createNewNote } = require('./lib/notes');
 
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
 // Features that allow the client to access the public folder as well as folders inside that.
@@ -15,43 +15,43 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
-// Main API endpoint
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-}); 
+// // Main API endpoint
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes);
+// }); 
 
 // Get notes based on their unique ID
-app.get('/api/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-})
+// app.get('/api/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, notes);
+//     if (result) {
+//         res.json(result);
+//     } else {
+//         res.send(404);
+//     }
+// })
 
-// API endpoint for POST requests
-app.post('/api/notes', (req, res) => {
-    req.body.id = uuid.v4();
-    console.log(notes);
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+// // API endpoint for POST requests
+// app.post('/api/notes', (req, res) => {
+//     req.body.id = uuid.v4();
+//     console.log(notes);
+//     const note = createNewNote(req.body, notes);
+//     res.json(note);
 
-});
+// });
 
 // HTML route to direct user to serve the notes.html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-// API request for deleting entries
-app.delete('/api/notes/:id', (req, res) => {
-    removeNote(req.params.id, notes);
-    res.json();
-});
+// // API request for deleting entries
+// app.delete('/api/notes/:id', (req, res) => {
+//     removeNote(req.params.id, notes);
+//     res.json();
+// });
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, './public/index.html'));
