b68ae4a9470c61c208b8fa9acd78da0153ee92fe
D1sl <94911312+D1sl@users.noreply.github.com>
2022-03-07 21:47:54 -0600

    added comments

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const { findById, removeNote, createNewNote } = require('./lib/notes');
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
+// Features that allow the client to access the public folder as well as folders inside that.
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,10 +18,12 @@ app.use(express.json());
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
+// Main API endpoint
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 }); 
 
+// Get notes based on their unique ID
 app.get('/api/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
@@ -30,6 +33,7 @@ app.get('/api/notes/:id', (req, res) => {
     }
 })
 
+// API endpoint for POST requests
 app.post('/api/notes', (req, res) => {
     req.body.id = uuid.v4();
     console.log(notes);
@@ -38,10 +42,12 @@ app.post('/api/notes', (req, res) => {
 
 });
 
+// HTML route to direct user to serve the notes.html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
+// API request for deleting entries
 app.delete('/api/notes/:id', (req, res) => {
     removeNote(req.params.id, notes);
     res.json();
@@ -51,13 +57,12 @@ app.delete('/api/notes/:id', (req, res) => {
 //     res.sendFile(path.join(__dirname, './public/index.html'));
 // })
 
-// // Wildcard
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// })
+// Wildcard - serves index.html when the user enters an address that doesn't match any of the other HTML routes.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
-
-
+// Sets up the port for the project
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 })
